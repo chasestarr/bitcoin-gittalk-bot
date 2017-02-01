89238386f21535aa4e630b2d4ec972344c6f4c4c
Chase Starr <chasestarr@gmail.com>
2017-01-31 18:41:52 -0800

    update to standard js formatting

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
-const request = require('request');
-const express = require('express');
+const request = require('request')
+const express = require('express')
 
-const app = express();
+const app = express()
 
 app.post('/', (req, res) => {
-  req.on('data', handleInbound);
-  res.status(200).end();
-});
+  req.on('data', handleInbound)
+  res.status(200).end()
+})
 
-function handleInbound(data) {
-  const inbound = JSON.parse(data.toString());
-  if (inbound.text.substring(0,8).toLowerCase() === '/bitcoin') {
-
+function handleInbound (data) {
+  const inbound = JSON.parse(data.toString())
+  if (inbound.text.substring(0, 8).toLowerCase() === '/bitcoin') {
     makeCoindeskRequest(inbound.room, (err, outboundPayload) => {
       postToGitTalk(outboundPayload, err => {
-        if (err) console.log(err);
-      });
-    });
+        if (err) console.log(err)
+      })
+    })
   }
 }
 
-function makeCoindeskRequest(room, cb) {
+function makeCoindeskRequest (room, cb) {
   request(`http://api.coindesk.com/v1/bpi/currentprice.json`, (err, respose, body) => {
-    if (err) return cb(err, null);
-    const data = JSON.parse(body);
-    const date = data.time.updated;
+    if (err) return cb(err, null)
+    const data = JSON.parse(body)
+    const date = data.time.updated
 
-    const price = data.bpi.USD.rate;
+    const price = data.bpi.USD.rate
     const outbound = {
       apiKey: '688b32f2675d0e03d2e0696a74237702',
       method: 'message',
@@ -35,18 +34,17 @@ function makeCoindeskRequest(room, cb) {
         text: `Current Bitcoin price in USD: $__${price}__`,
         avatar: 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/bitcoin-icon.png'
       }
-    };
+    }
 
-    cb(null, outbound);
-  });
+    cb(null, outbound)
+  })
 }
 
-function postToGitTalk(outbound, cb) {
+function postToGitTalk (outbound, cb) {
   request.post({ url: 'http://localhost:8000/apps', json: outbound }, (err, response, body) => {
-    if (err) return cb(err);
-    cb(null);
-  });
+    if (err) return cb(err)
+    cb(null)
+  })
 }
 
-app.listen(8003, () => console.log('server listening on port 8003'));
-
+app.listen(8003, () => console.log('server listening on port 8003'))
